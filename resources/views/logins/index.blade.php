5fff49c6edd7214798df80b244b18cf16f4e1d80
chinen-eita-FTC <e_chinen@ft-cons.co.jp>
2019-05-16 13:31:13 +0000

    外部API利用箇所の雛形を作成

--- resources/views/logins/index.blade.php
+++ resources/views/logins/index.blade.php
@@ -5,31 +5,10 @@
 @section('script')
 {{--  TODO[Ph.1.0] styleを外部ファイルに移し替えること --}}
 <style>
-.main-wrap{
-  display: flex;
-  justify-content: center;
-}
-
-.main-one-column {
-  width: 1080px;
-  min-width: 980px;
-  padding: 1.2em;
-  min
-}
 .login-box {
   width: 60%;
   margin: 0 auto;
   height: auto;
-  border: 1px solid #003268;
-  border-radius: 0.4em;
-}
-.login-title {
-  border-radius: 0.1em;
-  background: #003268; 
-  font-size: 2em;
-  font-weight: 500;
-  color: #fff;
-  text-align: center;
 }
 
 .login-form ul {
@@ -94,8 +73,8 @@
 @section('main')
 <div class="main-wrap">
   <div class="main-one-column">
+    <h1>ログイン画面</h1>
     <div class="login-box">
-      <h1 class="login-title">FLMログイン画面</h1>
       <form class="login-form" action="/login" method="post">
         {{ csrf_field() }}
         <ul>
