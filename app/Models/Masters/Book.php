4700fc1fcf1b39b3140b5927f2fad4a5e43b8783
takafumi-kurosaki-FTC <t_kurosaki@ft-cons.co.jp>
2019-05-21 09:40:49 +0000

    蔵書を1件削除する処理を追加

---
+++ app/Models/Masters/Book.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models\Masters;
+
+use App\Models\Model;
+use Illuminate\Support\Collection;
+use Log;
+use Exception;
+
+/**
+ * 蔵書モデル
+ *
+ * @package App\Models\Masters
+ */
+class Book extends Model
+{
+
+    /**
+     * テーブルの物理名
+     *
+     * @var string
+     */
+    protected $table = 'm_books';
+
+    /**
+     * 意図しないデータの更新を防ぎたいカラム群
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'id',
+    ];
+
+    /**
+     * モデルを配列/JSON変換するときに隠蔽されるカラム群
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * 主キーを指定して蔵書情報を削除
+     * 
+     * @param int $id 主キー
+     * @return Collection
+     */
+    public function deleteBook(int $id):Collection
+    {
+        $response['status'] = false;
+        try{
+            $target = $this->find($id);
+            $deleted = $target->delete();
+            $response['status'] = $deleted;
+            return collect($response);
+        } catch (Exception $e) {
+            // TODO [v1.0|機能追加] 例外処理の送出方法の決定後に削除時の例外処理の追加すること 
+        }
+    }
+}
