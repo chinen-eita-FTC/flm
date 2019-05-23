d5dabfc73a68d4f085761f25d29c004c87dc7f4c
takafumi-kurosaki-FTC <t_kurosaki@ft-cons.co.jp>
2019-05-23 06:29:35 +0000

    蔵書ジャンルマスタの削除機能の追加

--- app/Models/Masters/BookGenre.php
+++ app/Models/Masters/BookGenre.php
@@ -1,10 +1,57 @@
 <?php
+declare(strict_types=1);
 
-namespace App;
+namespace App\Models\Masters;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Model;
+use Illuminate\Support\Collection;
+use Log;
+use Exception;
+
+/**
+ * 蔵書モデル
+ *
+ * @package App\Models\Masters
+ */
 
 class BookGenre extends Model
 {
-    //
+
+    /**
+     * テーブルの物理名
+     *
+     * @var string
+     */
+    protected $table = 'm_book_genres';
+
+    /**
+     * 意図しないデータの更新を防ぎたいカラム群
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'id',
+    ];
+
+    /**
+     * モデルを配列/JSON変換するときに隠蔽されるカラム群
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    public function deleteBookGenre(int $id){
+        $response['status'] = false;
+        try{
+            $target = $this->find($id);
+            $deleted = $target->delete();
+            $response['status'] = $deleted;
+            return collect($response);
+        } catch (Exception $e) {
+            // TODO [v1.0|機能追加] 例外処理の送出方法の決定後に削除時の例外処理の追加すること 
+        }
+    }
+
 }
---
+++ tests/Unit/Models/Masters/BookGenreTest.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\Models\Masters;
+
+use Tests\Unit\Models\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Masters\BookGenre;
+
+/**
+ * 蔵書ジャンルモデルのテスト
+ *
+ * @package Tests\Unit\Models\Masters
+ */
+class BookGenreTest extends TestCase
+{
+
+    /**
+     * @var BookGenre テスト対象クラス
+     */
+    private $testee;
+
+    /**
+     * 初期処理
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->testee = app()->make(BookGenre::class);
+    }
+
+    /**
+     * @test
+     */
+    public function 任意の主キーを使用してレコードが1件削除されること()
+    {
+        // テストデータを準備
+        $this->testee->create(['name'=>'kurosaki']);
+        /* $factoryKey = 'デフォルト';
+        $seedingCount = 10;
+        $this->seed(
+            BookGenre::class,
+            $factoryKey,
+            $seedingCount
+        );*/
+
+        // 検証に使用する主キー
+        $id = 1;
+
+        // 返却値の検証
+        $actual = $this->testee->deleteBookGenre($id);
+        $this->assertTrue($actual->get('status'));
+
+        // DBの実体の検証
+        $actual = $this->testee->find($id);
+        $this->assertEmpty($actual);
+    }
+}
